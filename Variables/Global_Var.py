1f746d395f92f11ce8bffc1e50d124b779b9c834
Ash515 <aaashwin515@gmail.com>
2020-11-27 19:48:04 +0530

    Updated Global_Var file

--- Variables/Global_Var.py
+++ Variables/Global_Var.py
@@ -8,3 +8,20 @@ global_var() #calling the function
 """
 output become 11
 """
+"""
+Global variables for Nested functions
+"""
+def func_out():
+    x=10
+def func_in():
+    global x
+    x=20
+    print(x)
+func_in()
+
+"""
+output 20
+Because here we already declare x=10 to func_out() so there is no need to make that x as global in 
+func_in().
+"""
+
