7f6ab1fa9c1a21a5565198f62da5b626521f21aa
Ashwin Kumar Ramaswamy <aaashwin515@gmail.com>
2021-01-23 23:19:09 +0530

    updated arm strong.py

--- Arm Strong Number.py
+++ Arm Strong Number.py
@@ -1,12 +1,23 @@
 print("To check wheather the given number is arm strong or not ")
-num=int(input("Enter the number:"))
-temp=num
-sum=0
-while temp!=0:
-    digit=temp%10
-    sum+=digit**3
-    temp//=10
-if num==sum:
-    print("The given {} number is palindrome".format(num))
+n=int(input("Enter the 3 digt values:"))
+val=[int(d) for d in str(n)]
+#val=[1,5,3]
+c=0
+l=[]
+for i in val:
+    c=i**3
+   
+    l.append(c)
+a=0
+for i in l:
+    a=a+i
+
+
+if(a==n):
+    print("It is an Arm strong ")
 else:
-    print("The given {} number is not a palindrome".format(num))
+    print("not an arm strong")
+
+
+
+    
