98643eba6d8cf0feddb867483ae1447d2aea6d39
Ashwin Kumar Ramaswamy <aaashwin515@gmail.com>
2021-02-09 18:43:43 +0530

    Update Looping.py

--- Control Structures/Looping.py
+++ Control Structures/Looping.py
@@ -1,11 +1,12 @@
 #Lopping staements are used to reduce the writing code
-#code to print n natural numbers
+#code to print n natural numbers using for loop
 print("To print the n natural numbers")
 n=int(input("Enter the total value:"))
 count=0
 for i in range(n):
     count=count+1
     print("The values are ,"count)
+
 
 #While
 #To print values
